fc7ed1870296ae2f542b09fadb202137d52786a0
tshadat2002 <tshadat2968@bths.edu>
2022-07-31 13:37:21 -0400

    finished

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,21 @@ const express = require("express"),
 const bodyParser = require('body-parser');
 const fs = require("fs");
 
-app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.listen(port, () => console.log("Backend server live on " + port));
+
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
 
 app.get("/", (req, res) => {
     res.send({ message: "Connected to Backend server!" });
@@ -65,3 +77,21 @@ app.get("/items/search",searchItems)
     //console.log(returnData);
     response.json(returnData);
   }
+
+app.get("/authenticate", auth, (req, res) => {
+  console.log(`user logging in: ${req.auth.user}`);
+  res.cookie('user', req.auth.user, { signed: true });
+  res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const upsertSucceeded = upsertUser(username, password)
+  res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie('user');
+  res.end();
+});
